2db3b633d8a9f6c13d4c01aaee3f320d4626e2dc
serbulas1 <serbulas1@gmail.com>
2020-11-10 17:46:48 +0100

    create presentation controller

---
+++ shlidexperience/Controllers/PresentationController.cs
@@ -0,0 +1,16 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using shlidexperience.Controllers;
+
+namespace Api.Controllers
+{
+    [Route("api/presentations")]
+    [ApiController]
+    public class PresentationController : BaseController
+    {
+        public PresentationController(IOptions<AppSettings> options) : base(options)
+        {
+        }
+    }
+}
