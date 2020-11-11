4fdf39bd6daebcb065418c56fe57f34e405ae219
serbulas1 <serbulas1@gmail.com>
2020-11-11 11:05:37 +0100

    create slide controller

---
+++ shlidexperience/Controllers/SlideController.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using shlidexperience.Controllers;
+
+namespace Api.Controllers
+{
+    [Route("api/slides")]
+    [Authorize]
+    public class SlideController : BaseController
+    {
+        public SlideController(IOptions<AppSettings> options) : base(options)
+        {
+
+        }
+    }
+}
