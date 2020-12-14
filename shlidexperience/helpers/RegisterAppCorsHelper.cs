d78b0f11ba47700101446cd025e95c3b6c4ed3ca
serbulas1 <serbulas1@gmail.com>
2020-12-14 18:14:13 +0100

    add allowCredentials so signal R can work

--- shlidexperience/helpers/RegisterAppCorsHelper.cs
+++ shlidexperience/helpers/RegisterAppCorsHelper.cs
@@ -17,6 +17,7 @@ namespace shlidexperience.helpers
                     builder.WithOrigins(urls);
                     builder.AllowAnyHeader();
                     builder.AllowAnyMethod();
+                    builder.AllowCredentials();
                 });
             });
             return services;
